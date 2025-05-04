9c1c85fcc18dcbc547884e51a84a5c53569cc996
Inkentinken67 <isen.axel@gmail.com>
2025-05-04 20:37:36 +0200

    daddsd

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask
 import os
 
 app = Flask(__name__)
 
-@app.route('/api/hello')
-def hello():
-    return jsonify({"message": "Hello from Flask!"})
+@app.route('/')
+def home():
+    return 'Hello from Flask on Render!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
